4e254d9333bb55886557c3eeab59371255604ea5
yunzhen <73164267+yunzhxn@users.noreply.github.com>
2024-07-20 13:42:29 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v C:\\Documents\\Github\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\USER\\Documents\\GitHub\\jenkins-php-selenium-testsrc:/var/www/html php:7.2-apache
 sleep 1
 set +x
 echo 'Now...'
